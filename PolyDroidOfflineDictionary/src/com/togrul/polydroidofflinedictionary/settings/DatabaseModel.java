31de7df6060606b31c2fa88d8ff48ea409c11179
togrul_seyidov <togrul_seyidov@localhost>
2014-06-27 01:33:22 +0500

    DatabaseModel

--- PolyDroidOfflineDictionary/src/com/togrul/polydroidofflinedictionary/settings/DatabaseModel.java
+++ PolyDroidOfflineDictionary/src/com/togrul/polydroidofflinedictionary/settings/DatabaseModel.java
@@ -5,51 +5,62 @@ public class DatabaseModel {
 	private String id;
 	private String size;//
 	private String name; //
-	private String dbname;
-	private String download; //new SpinnerAdapter(activity).isDownloaded(Stringeger.valueOf(XMLfunctions.getValue(e, "id")))+ "")
+	private String dbName;
+//	private boolean download; // new pinnerAdapter(activity).isDownloaded(Stringeger.valueOf(XMLfunctions.getValue(e, "id")))+ "")
 	private boolean isdownload = false; //
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getSize() {
 		return size;
 	}
+
 	public void setSize(String size) {
 		this.size = size;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getDbname() {
-		return dbname;
+
+	public String getDbName() {
+		return dbName;
 	}
-	public void setDbname(String dbname) {
-		this.dbname = dbname;
+
+	public void setDbname(String dbName) {
+		this.dbName = dbName;
 	}
-	public String getDownload() {
-		return download;
-	}
-	public void setDownload(String download) {
-		this.download = download;
-	}
-	public boolean getIsdownload() {
+
+//	public String getDownload() {
+//		return download;
+//	}
+//
+//	public void setDownload(String download) {
+//		this.download = download;
+//	}
+
+	public boolean getIsDownload() {
 		return isdownload;
 	}
-	public void setIsdownload(boolean isdownload) {
+
+	public void setIsDownload(boolean isdownload) {
 		this.isdownload = isdownload;
-	}	
+	}
 	
 	@Override
 	public String toString() {
 		return "DatabaseModel [id=" + id + ", size=" + size + ", name=" + name
-				+ ", dbname=" + dbname + ", download=" + download
+				+ ", dbName=" + dbName /*+ ", download=" + download*/
 				+ ", isdownload=" + isdownload + "]";
 	}
 }
